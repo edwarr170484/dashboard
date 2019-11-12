770d2e1d62fcc71a992fa73603deb45937f202dc
Эдгар Жизневский <edwarr170484@gmail.com>
2019-11-12 22:52:55 +0300

    Инструменты управления в сообщениях личного кабинета

--- www/bundles/Default/js/customNew.js
+++ www/bundles/Default/js/customNew.js
@@ -32,3 +32,50 @@ function radioSetActive(element){
     element.parent().find('li').each(function(){$(this).find('a').removeClass('active');});
     element.find('a').addClass('active');
 }
+
+function selectConversations(items, element){
+    element.parent().parent().find('a').removeClass('active');
+    element.addClass('active');
+    
+    var conversations = $(".accountMessages").find(".accountMessageItem");
+    conversations.each(function(){$(this).removeClass('hide');});
+    
+    if(items == 'active'){
+        conversations.each(function(){
+            if($(this).hasClass('active')){
+                
+            }else{
+                $(this).addClass('hide');
+            }
+        });
+    }
+    
+    if(items == 'readed'){
+        conversations.each(function(){
+            if($(this).hasClass('active')){
+                $(this).addClass('hide');
+            }
+        });
+    }
+}
+
+function selectAll(element){
+    var checked = element.prop("checked");
+    var parent = $(".accountMessages");
+    parent.find(".custom-checkbox").each(function(){    
+        if( checked == false)
+        {
+            $(this).prop("checked", false);
+            $(this).parent().removeClass('active');
+            $(this).next('.checkbox-cover-inner').removeClass('active');
+        }
+        else
+        {
+            $(this).prop("checked", true);
+            if(!$(this).parent().hasClass('active')){
+                $(this).parent().addClass('active');
+                $(this).next('.checkbox-cover-inner').addClass('active');
+            }
+        }                
+    });
+}

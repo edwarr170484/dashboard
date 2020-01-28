c8bbc7cf7a663beee2892010819c6bbe0fd9f9f8
Эдгар Жизневский <edwarr170484@gmail.com>
2020-01-28 22:39:22 +0300

    Paypal verification

--- src/Dashboard/CommonBundle/Controller/PaypalController.php
+++ src/Dashboard/CommonBundle/Controller/PaypalController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
@@ -39,7 +40,27 @@ class PaypalController extends Controller
         $client = PayPalClient::client();
         $response = $client->execute(new OrdersGetRequest($request->request->get('orderId')));
         
-        return new Response();
+        if($response->statusCode == '200'){
+            if($response->result->status == 'COMPLETED'){
+                $invoice = $response->result->purchase_units[0]->amount;
+                $billId = $response->result->purchase_units[0]->invoice_id;
+                $bill = $manager->getRepository("DashboardCommonBundle:Bill")->find($billId);
+                
+                $locale = $manager->getRepository("DashboardCommonBundle:Locale")->findOneBy(array("code" => $request->getLocale()));
+
+                if($invoice->currency_code == $locale->getCurrency()->getCode() && $bill){
+                    if($bill->getPrice() == $invoice->value){
+                        $bill->setDatePayed(new \DateTime($response->result->update_time));
+                        $bill->setIsPayed(1);
+                        $manager->persist($bill);
+                        //$manager->flush();
+                        return new JsonResponse(array("error" => 0, "redirect" => "/account/payment/success/" . $billId));
+                    }
+                }
+            }
+        }
+        
+        return new JsonResponse($response);
     }
 }
 
